0fb9e03c8abb83f5b17595e5a1e52cfe74f49b17
Anton Marchanka <amarcha1@jaguarlandrover.com>
2023-03-13 00:09:27 +0400

    minor code clean up

--- GPTChat/Modules/NewChatView.swift
+++ GPTChat/Modules/NewChatView.swift
@@ -14,71 +14,78 @@ struct NewChatView: View {
     @EnvironmentObject private var model: AppModel
 
     @Environment(\.dismiss) var dismiss
-
+    @FocusState private var fieldIsFocused: Bool
     var body: some View {
         NavigationStack {
-            VStack(spacing: 32){
-                VStack {
-                    HStack {
-                        TextField("Required", text: $model.newChatEntryText, axis: .vertical)
-                            .padding(8)
-                            .background(Color(.secondarySystemFill).cornerRadius(10))
-                            .padding(.leading, 24)
+            ScrollView {
+                VStack(spacing: 32){
+                    VStack {
+                        HStack {
+                            TextField("Required", text: $model.newChatEntryText, axis: .vertical)
+                                .padding(8)
+                                .background(Color(.secondarySystemFill).cornerRadius(10))
+                                .padding(.leading, 24)
 
-                        if model.newChatEntryText.isEmpty {
-                            Button("Paste") {
-                                model.newChatEntryText = UIPasteboard.general.string ?? ""
+                            if model.newChatEntryText.isEmpty {
+                                Button("Paste") {
+                                    model.newChatEntryText = UIPasteboard.general.string ?? ""
+                                }
+                                .padding(.trailing, 24)
                             }
-                            .padding(.trailing, 24)
+                        }
+                        Text("Provide text")
+                            .font(.system(size: 13))
+                            .foregroundColor(.gray)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 34)
+                    }
+
+                    if model.isEmptyNewChatScreen, !model.newChatEntryText.isEmpty {
+                        Text("Tap 'send' in the top right corner of your screen")
+                            .font(.system(size: 18))
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                    }
+
+                    if model.isThinking {
+                        VStack {
+                            Text("Generating response...")
+                            ProgressView().progressViewStyle(.circular)
                         }
                     }
-                    Text("Provide text")
-                        .font(.system(size: 13))
-                        .foregroundColor(.gray)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal, 34)
-                }
-
-                if model.isEmptyNewChatScreen, !model.newChatEntryText.isEmpty {
-                    Text("Tap 'send' in the top right corner of your screen")
-                        .font(.system(size: 18))
-                        .foregroundColor(.gray)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                }
-
-                if model.isThinking {
-                    VStack {
-                        Text("Generating response...")
-                        ProgressView().progressViewStyle(.circular)
+                    if model.hasResultNewChatScreen {
+                        Text(model.generatedNewChat)
                     }
-                }
-                if model.hasResultNewChatScreen {
-                    Text(model.generatedNewChat)
                 }
             }
+            .scrollDismissesKeyboard(.immediately)
             .navigationTitle(model.selectedModule?.title ?? "")
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    HStack{
-                        Button("Close") {
-                            dismiss()
-                        }
+            .toolbar { screenToolbar }
+        }
+    }
 
-                        if !model.generatedNewChat.isEmpty {
-                            Button("Reset") {
-                                model.newChatEntryText = ""
-                                model.generatedNewChat = ""
-                            }
+    private var screenToolbar: some ToolbarContent {
+        Group {
+            ToolbarItem(placement: .navigationBarLeading) {
+                HStack{
+                    Button("Close") {
+                        dismiss()
+                    }
+
+                    if !model.generatedNewChat.isEmpty {
+                        Button("Reset") {
+                            model.newChatEntryText = ""
+                            model.generatedNewChat = ""
                         }
                     }
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Send") {
-                        model.makeNewChat()
-                    }
-                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Send") {
+                    model.makeNewChat()
+                }.disabled(model.isThinking || model.newChatEntryText.isEmpty)
             }
         }
     }
